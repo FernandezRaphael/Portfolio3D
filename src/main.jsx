2b007534c6430b45defaf8144fd5f44b9274bc7f
raphael <fernandezraphael59@gmail.com>
2024-02-01 12:47:51 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Room from './Room.jsx'
 import './index.css'
 import Camera from './Camera.jsx'
 
